6ef576bf25bc9a9fd9bca8c83b76284ab80c2210
ganeshkarthik09 <vislavathganeshkarthik@gmail.com>
2026-02-03 22:32:03 +0530

    Solved 2 more 800 rated from CP-31(25-26)

---
+++ C++/Competitive Programming/CF Daily/TLE Eliminators/Cp-800/C. Prepend and Append.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int left = 0, right = n - 1;
+    while (left <= right) {
+      if ((s[left] == '0' && s[right] == '1') ||
+          (s[left] == '1' && s[right] == '0')) {
+        left++, right--;
+      } else {
+        break;
+      }
+    }
+    cout << right - left + 1 << endl;
+  }
+  return 0;
+}
